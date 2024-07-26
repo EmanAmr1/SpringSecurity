e3c5cf615b70fc4085df3f5b6d7ede63eed0af3f
EmanAmr1 <emanamr543@gmail.com>
2024-07-26 21:04:28 +0300

    UserDetailsService

--- security/src/main/java/com/spring/security/config/JwtAuthenticationFilter.java
+++ security/src/main/java/com/spring/security/config/JwtAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,7 +20,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
 
     private final JwtService jwtService;
-
+    private final UserDetailsService userDetailsService; //implemented service from org.springframework.security.core.userdetails.UserDetailsService
+                                                         //should make bean for it in configApp
     @Override
     protected void doFilterInternal(@NonNull   HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
@@ -36,6 +40,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
         jwt=authHeader.substring(7);
 
-        userEmail=  jwtService.extractUsername(jwt);       //extract user email
+        userEmail=  jwtService.extractUsername(jwt);//extract user email
+
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){ //mat3mlho4 auth before
+
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+        }
+
     }
 }
