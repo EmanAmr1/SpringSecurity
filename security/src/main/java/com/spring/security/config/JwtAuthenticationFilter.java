2d7350624fa5751219afad47f5e193f828bcb5c9
EmanAmr1 <emanamr543@gmail.com>
2024-07-26 21:22:53 +0300

    Check if the email is provided and no authentication is present

--- security/src/main/java/com/spring/security/config/JwtAuthenticationFilter.java
+++ security/src/main/java/com/spring/security/config/JwtAuthenticationFilter.java
@@ -6,13 +6,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.net.http.WebSocket;
 
 @Component
 @RequiredArgsConstructor
@@ -42,11 +45,36 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
         userEmail=  jwtService.extractUsername(jwt);//extract user email
 
-        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){ //mat3mlho4 auth before
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) { //mat3mlho4 auth before
 
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail); //get user
 
+           if(jwtService.isTokenValid(jwt ,userDetails)) { //check token valid
+
+               UsernamePasswordAuthenticationToken authToken // make obj to get user & getAuthorities
+                       = new UsernamePasswordAuthenticationToken
+                       (userDetails,
+                               null,
+                               userDetails.getAuthorities()
+                       );
+
+               authToken // enforce auth obj with details of our request
+                       .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+               //Update the SecurityContextHolder //make this user (obj) auth done to not auth after this
+               SecurityContextHolder.getContext().setAuthentication(authToken);
+
+           }
         }
-
+        filterChain.doFilter(request,response);
     }
 }
+/*
+Steps:
+Check if the email is provided and no authentication is present.
+Load user details using the email.
+Validate the token with the user details.
+Create an authentication token with user details.
+Set additional request details.
+Update the security context with the new authentication token
+ */
