2c926ec1eca397f572065591783905de32e79949
EmanAmr1 <emanamr543@gmail.com>
2024-07-26 18:42:04 +0300

    Implement User Entity Class

---
+++ security/src/main/java/com/spring/security/user/User.java
@@ -0,0 +1,32 @@
+package com.spring.security.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Integer Id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+
+}
+
