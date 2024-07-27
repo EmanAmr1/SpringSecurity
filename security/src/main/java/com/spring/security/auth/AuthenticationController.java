1412eb2326eb1b69e8d3f8778003646a86d0b044
EmanAmr1 <emanamr543@gmail.com>
2024-07-27 17:50:49 +0300

    fix issue of secured endpoint

--- security/src/main/java/com/spring/security/auth/AuthenticationController.java
+++ security/src/main/java/com/spring/security/auth/AuthenticationController.java
@@ -3,11 +3,7 @@ package com.spring.security.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -27,6 +23,8 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
+
+
 
 }
 
--- security/src/main/java/com/spring/security/demo/DemoController.java
+++ security/src/main/java/com/spring/security/demo/DemoController.java
@@ -4,10 +4,12 @@ package com.spring.security.demo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("/api/v1/demo-controller")
+@RestController
+@RequestMapping("/api/v1/demo")
 public class DemoController {
 
     @GetMapping
